36906209b10bee7030054300a177ca26cf8758e4
Rahul T <rahulmanoj@cet.ac.in>
2020-11-08 21:18:54 +0530

    error fixed

--- learn/urls.py
+++ learn/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('learn2')),
+    path('',include('learn2.urls')),
 ]
--- learn/views.py
+++
@@ -1,7 +0,0 @@
-from django.shotcuts import render
-
-def index(request):
-    return render(request, 'index.html'),
-def main(request, username):
-    return render(request, 'main.html')
-    "username" : username
